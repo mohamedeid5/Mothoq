cea140a2d5a219a7747abf6e03fad540ab755c8d
mohamed eid <meid368@gmail.com>
2026-09-12 05:55:23 +0300

    fix: allow BuildKit to inspect ECR images

--- infra/terraform/modules/identity/main.tf
+++ infra/terraform/modules/identity/main.tf
@@ -158,6 +158,7 @@ resource "aws_iam_role_policy" "github_actions" {
         Effect = "Allow"
         Action = [
           "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
           "ecr:CompleteLayerUpload",
           "ecr:InitiateLayerUpload",
           "ecr:PutImage",
